8e7cba3cb194540cff9932073e79ab5d608ba65b
karimelghazouly <karimelghazouly@gmail.com>
2018-12-19 13:52:28 +0200

    Level design

--- Doofs/Assets/scripts/move.cs
+++ Doofs/Assets/scripts/move.cs
@@ -24,6 +24,14 @@ public class move : MonoBehaviour {
         if (Input.GetKey(KeyCode.S))
             transform.Translate(new Vector3(0f,0f,-speed * Time.deltaTime));
 
+        if (Input.GetKey(KeyCode.RightArrow))
+        transform.Rotate(new Vector3(0f, 7f, 0f));
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(new Vector3(0f, -7f, 0f));
+
+            
+       
+
 		
 	}
 }
